d9fedde4500489882357ce40a0dd13758abe2497
metheyes <31736775+metheyes@users.noreply.github.com>
2017-10-24 01:53:33 +0200

    minimum points

--- tw_bb.js
+++ tw_bb.js
@@ -1,7 +1,7 @@
 $.get("/map/village.txt", function(data) {
-	console.log(data);
+	let points = prompt("Minimum poäng på barbarbyarna (standard: 1000)", "1000") || 1000;
 	let lista = [];
 	let b = data.split("\n");
-	$.each(b, (i) => { let inf = b[i].split(","); if(inf[1] === "Barbarby"){ lista.push("<a href=\"/game.php?screen=info_village&id=" + inf[0] + "#" + inf[2] + ";" + inf[3] + "\">" + inf[2] + "|" + inf[3] + "</a> " + inf[5] + "p<br />"); } });
+	$.each(b, (i) => { let inf = b[i].split(","); if(inf[1] === "Barbarby" && inf[5] >= points){ lista.push("<a href=\"/game.php?screen=info_village&id=" + inf[0] + "#" + inf[2] + ";" + inf[3] + "\">" + inf[2] + "|" + inf[3] + "</a> " + inf[5] + "p<br />"); } });
 	$("table[class=content-border]").before(lista);
 }, "text");
