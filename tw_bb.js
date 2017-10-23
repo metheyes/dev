0c422d86ea935329abe6365bb51a6304feb55980
metheyes <31736775+metheyes@users.noreply.github.com>
2017-10-24 01:48:53 +0200

    added points

--- tw_bb.js
+++ tw_bb.js
@@ -2,6 +2,6 @@ $.get("/map/village.txt", function(data) {
 	console.log(data);
 	let lista = [];
 	let b = data.split("\n");
-	$.each(b, (i) => { let inf = b[i].split(","); if(inf[1] === "Barbarby"){ lista.push("<a href=\"/game.php?screen=info_village&id=" + inf[0] + "#" + inf[2] + ";" + inf[3] + "\">" + inf[2] + "|" + inf[3] + "</a><br />"); } });
+	$.each(b, (i) => { let inf = b[i].split(","); if(inf[1] === "Barbarby"){ lista.push("<a href=\"/game.php?screen=info_village&id=" + inf[0] + "#" + inf[2] + ";" + inf[3] + "\">" + inf[2] + "|" + inf[3] + "</a> " + inf[5] + "p<br />"); } });
 	$("table[class=content-border]").before(lista);
 }, "text");
