1619aed9ab22c449a3ea5d2cddd4d6553ce893e8
metheyes <31736775+metheyes@users.noreply.github.com>
2017-10-24 01:46:45 +0200

    update :D

--- tw_bb.js
+++ tw_bb.js
@@ -3,5 +3,5 @@ $.get("/map/village.txt", function(data) {
 	let lista = [];
 	let b = data.split("\n");
 	$.each(b, (i) => { let inf = b[i].split(","); if(inf[1] === "Barbarby"){ lista.push("<a href=\"/game.php?screen=info_village&id=" + inf[0] + "#" + inf[2] + ";" + inf[3] + "\">" + inf[2] + "|" + inf[3] + "</a><br />"); } });
-	alert(lista);
+	$("table[class=content-border]").before(lista);
 }, "text");
