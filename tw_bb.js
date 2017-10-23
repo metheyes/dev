727c86ee83e00f1b5587215deed3e1e33327dcbd
metheyes <31736775+metheyes@users.noreply.github.com>
2017-10-24 01:26:42 +0200

    barbarfinder.js

---
+++ tw_bb.js
@@ -0,0 +1,10 @@
+$.ajax({
+	method: "GET",
+	url: "/map/village.txt"
+}).done(function(data) {
+	console.log(data);
+});/*
+let lista = [];
+let b = $('body').html().split("\n");
+$.each(b, (i) => { let inf = b[i].split(","); if(inf[1] === "Barbarby"){ lista.push(inf[2] + "|" + inf[3] + "<br />"); } });
+$('body').html(lista);*/
