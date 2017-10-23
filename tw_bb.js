47d1ebd9e2dd3fd8bd3f331e951b5e6ecdbf1c47
metheyes <31736775+metheyes@users.noreply.github.com>
2017-10-24 01:57:50 +0200

    dev

--- tw_bb.js
+++ tw_bb.js
@@ -3,6 +3,12 @@ $.get("/map/village.txt", function(data) {
 	console.log(points);
 	let lista = [];
 	let b = data.split("\n");
-	$.each(b, (i) => { let inf = b[i].split(","); if(inf[1] === "Barbarby" && inf[5] >= points){ lista.push("<a href=\"/game.php?screen=info_village&id=" + inf[0] + "#" + inf[2] + ";" + inf[3] + "\">" + inf[2] + "|" + inf[3] + "</a> " + inf[5] + "p<br />"); } });
+	$.each(b, (i) => {
+		let inf = b[i].split(",");
+		if(inf[1] === "Barbarby" && inf[5] >= points) {
+			lista.push("<a href=\"/game.php?screen=info_village&id=" + inf[0] + "#" + inf[2] + ";" + inf[3] + "\">" + inf[2] + "|" + inf[3] + "</a> " + inf[5] + "p<br />");
+		}
+		console.log(inf[5] + " vs " + points);
+	});
 	$("table[class=content-border]").before(lista);
 }, "text");
