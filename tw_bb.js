62ccf62c4ee36c28b68ad83c97ad2d080b62015a
metheyes <31736775+metheyes@users.noreply.github.com>
2017-10-24 01:45:15 +0200

    fix

--- tw_bb.js
+++ tw_bb.js
@@ -1,10 +1,7 @@
-$.ajax({
-	//method: "GET",
-	url: "/map/village.txt"
-}).done(function(data) {
+$.get("/map/village.txt", function(data) {
 	console.log(data);
-});/*
-let lista = [];
-let b = $('body').html().split("\n");
-$.each(b, (i) => { let inf = b[i].split(","); if(inf[1] === "Barbarby"){ lista.push(inf[2] + "|" + inf[3] + "<br />"); } });
-$('body').html(lista);*/
+	let lista = [];
+	let b = data.split("\n");
+	$.each(b, (i) => { let inf = b[i].split(","); if(inf[1] === "Barbarby"){ lista.push("<a href=\"/game.php?screen=info_village&id=" + inf[0] + "#" + inf[2] + ";" + inf[3] + "\">" + inf[2] + "|" + inf[3] + "</a><br />"); } });
+	alert(lista);
+}, "text");
