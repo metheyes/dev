f3e868a5ec4407ceb0d690a5d490f40f2f2181d7
metheyes <31736775+metheyes@users.noreply.github.com>
2017-10-24 02:17:39 +0200

    fix för matchningskiten

--- tw_bb.js
+++ tw_bb.js
@@ -4,7 +4,7 @@ $.get("/map/village.txt", function(data) {
 	let b = data.split("\n");
 	$.each(b, (i) => {
 		let inf = b[i].split(",");
-		if(inf[1] === "Barbarby" && inf[5] >= points) {
+		if(inf[4] == 0 && inf[5] >= points) {
 			//console.log(inf);
 			lista.push("<tr><td>" + inf[1] + "</td><td>" + inf[5] + "p</td><td><a href=\"/game.php?screen=info_village&id=" + inf[0] + "#" + inf[2] + ";" + inf[3] + "\">" + inf[2] + "|" + inf[3] + "</a></td></tr>");
 		}
