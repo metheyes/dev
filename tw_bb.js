add9d98b5c659bcab93daeab90eec211caa14dab
metheyes <31736775+metheyes@users.noreply.github.com>
2017-10-24 02:14:54 +0200

    dev

--- tw_bb.js
+++ tw_bb.js
@@ -5,8 +5,8 @@ $.get("/map/village.txt", function(data) {
 	$.each(b, (i) => {
 		let inf = b[i].split(",");
 		if(inf[1] === "Barbarby" && inf[5] >= points) {
-			console.log(inf);
-			//lista.push("<tr><td>" + inf[1] + "</td><td>" + inf[5] + "p</td><td><a href=\"/game.php?screen=info_village&id=" + inf[0] + "#" + inf[2] + ";" + inf[3] + "\">" + inf[2] + "|" + inf[3] + "</a></td></tr>");
+			//console.log(inf);
+			lista.push("<tr><td>" + inf[1] + "</td><td>" + inf[5] + "p</td><td><a href=\"/game.php?screen=info_village&id=" + inf[0] + "#" + inf[2] + ";" + inf[3] + "\">" + inf[2] + "|" + inf[3] + "</a></td></tr>");
 		}
 	});
 	$("table[class=content-border]").append("<table class=\"vis\" style=\"width:50%\"><tbody><tr><th style=\"width:33%\>Bynamn</th><th style=\"width:33%\>Poäng</th><th style=\"width:33%\>Kordinater</th></tr>" + lista + "</tbody></table>");
